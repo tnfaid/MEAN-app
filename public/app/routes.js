b1e619370b2b3fd3cc00dc6d0c0e3e624bdb8c3d
tnfaid <nurfaidahtitik@gmail.com>
2017-07-18 17:34:45 +0700

    finish part 5

---
+++ public/app/routes.js
@@ -0,0 +1,23 @@
+angular.module('appRoutes',['ngRoute'])
+
+.config(function($routeProvider, $locationProvider){
+	
+	$routeProvider
+
+	.when('/', {
+		templateUrl : 'app/views/pages/home.html'
+	})
+
+	.when('/',{
+		templateUrl : 'app/views/pages/about.html'
+	})
+
+	.otherwise({ redirectTo : '/'});
+	$locationProvider.html5Mode({
+	  enabled: true,
+	  requireBase: false
+	});
+});
+
+
+
